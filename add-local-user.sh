b8286ed43e1f0b08e73f4c66f44b557c6a66eec8
BigTony <kejiezhang666@gmail.com>
2019-02-01 13:30:11 -0500

    feat: generate random password for new users

--- add-local-user.sh
+++ add-local-user.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #
 # This script creates a new user on the local system
-# You will be prompted to enter the username(login), the person name, and a password.
+# You can supply a username as an argument to the script, optionally you can also provide a comment for the account as an argument.
+# If arguments are ignored, you will be prompted to enter the username(login) and the person name.
+# A password will automatically generated for the account.
 # The username, password, and host for the account will be displayed
 
 # Make sure the script is being executed with superuser privileges.
@@ -11,14 +13,30 @@ then
   exit 1
 fi
 
-# Get the username(login)
-read -p "Enter the username to create: " USER_NAME
+if [[ "${#}" == 0 ]]; then
+  # Get the username(login)
+  read -p "Enter the username to create: " USER_NAME
 
-# Get the real name(contents for the description field)
-read -p "Enter the name of the person or application that will be using this account:" COMMENT
+  # Get the real name(contents for the description field)
+  read -p "Enter the name of the person or application that will be using this account:" COMMENT
 
-# Get the password
-read -p "Enter the password to use for the account: " PASSWORD
+else
+  if [[ "${1}" == "--help" ]] || [[ "${1}" == "-h" ]]; then
+    echo "Usage: ${0} [USER_NAME] [COMMENT...]"
+    echo "Create an account on the local system with the name of USER_NAME and a comments field of COMMENT"
+    exit 1
+  else
+    # The first parameter is the user name
+    USER_NAME="${1}"
+
+    # The rest of the parameters are for the account comments.
+    shift
+    COMMENT="${@}"
+  fi
+fi
+
+# Generate a password
+PASSWORD=$(date +%s%N | sha256sum | head -c10)
 
 # Create the account
 useradd -c "${COMMENT}" -m ${USER_NAME}
